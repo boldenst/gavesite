9d71993b4dbfb49afec185a820c02c18737af4d4
Hjorthex <74530828+Hjorthex@users.noreply.github.com>
2021-12-09 09:52:30 +0100

    Sign up system done

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-// var loginUnfold = document.querySelector('#auth-login-unfold');
-// var loginUnfolded = document.querySelector('.signup-container');
-
-// loginUnfold.addEventListener('click', function() {
-//     loginUnfolded.classList.add('unfolded');
-// })
 
 function toDashboard() {
     window.location.href = "./dashboard.html";
